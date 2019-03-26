6d7ce283e9f0882aa904a07083dc59fc3b42c6a6
andrew ha <haandyha@gmail.com>
2019-03-26 14:37:57 -0400

    remove exercise

--- angular-exercise/src/app/pipes/first-cap.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FirstCapPipe } from './first-cap.pipe';
-
-describe('FirstCapPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FirstCapPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
